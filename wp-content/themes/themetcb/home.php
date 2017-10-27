ee29737a4ced09a5ea7ebe52f4630639e09ae381
Kendall <kadkins@thecipherbrief.com>
2017-10-27 16:01:56 -0400

    meet our board final

--- wp-content/themes/themetcb/home.php
+++ wp-content/themes/themetcb/home.php
@@ -290,34 +290,42 @@
 
         <!-- Meet Our Board -->
         <div class="meet_board">
-          <?php /* <?php
-            $wpb_all_query = new WP_Query(array(
-              'post_type'=> 'meet',
-              'post_status'=> 'publish',
-              'posts_per_page'=> 1,
-            ));
-            ?>
+        <?php
+          $board = wp_count_posts('board')->publish;
+          $wpb_all_query = new WP_Query(array(
+            'post_type'=> 'board',
+            'post_status'=> 'publish',
+            'offset' => rand(0, $board - 1 ),
+            'showposts'=> 1
+          ));
+          ?>
           <?php if ( $wpb_all_query->have_posts() ) : ?>
               <!-- the loop -->
               <?php while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); ?>
+                <h3 style="font-weight: bold; text-align: center; line-height: 1.2; font-family: 'replica-pro';"><a href="https://www.thecipherbrief.com/cyber-advisory-board">Meet Our Board</a></h3>
+                <div class="meet_content" style="padding: 4px 4px 0 4px;">
+                  <a href="<?php the_permalink(); ?>" class="board_img"> <?php the_post_thumbnail( 'thecipherbrief-thumbnail-cab'); ?> </a>
+                  <p class="board_text"><?php the_truncated_post(290); ?></p>
+                </div>
+                <div class="board_link_holder">
+                    <a class="board_link" href="https://www.thecipherbrief.com/cyber-advisory-board" style=" display: block; text-align: center; color: #d52340;">View the rest of the Cyber Advisory Board</a>
+                </div>
+              </div>
               <?php endwhile; ?>
               <!-- end of the loop -->
               <?php wp_reset_postdata(); ?>
 
           <?php else : ?>
-              <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+            <h3 style="font-weight: bold; text-align: center; line-height: 1.2; font-family: 'replica-pro';"><a href="https://www.thecipherbrief.com/cyber-advisory-board">Meet Our Board</a></h3>
+            <div class="meet_content" style="padding: 4px 4px 0 4px;">
+              <img class="board_img" src="https://thecipherbriefcyber.com/wp-content/uploads/2017/10/Robert-Work-bw.jpg" alt="Robert Work">
+              <p class="board_text">Mr. Robert Work was the 31st Deputy Secretary of Defense, serving under three different Secretaries across two administrations. Previously, he served as chief executive officer of the Center for a New American Security. From 2009 to 2013, he served as the Under Secretary of the Navy. Mr. Work served on active duty in the U.S. Marine Corps for 27 years, retiring as a Colonel in 2001.</p>
+            </div>
+            <div class="board_link_holder">
+                <a class="board_link" href="https://www.thecipherbrief.com/cyber-advisory-board" style=" display: block; text-align: center; color: #d52340;">View the rest of the Cyber Advisory Board</a>
+            </div>
+          </div>
           <?php endif; ?>
-          */?>
-          <h3 style="font-weight: bold; text-align: center; line-height: 1.2; font-family: 'replica-pro';"><a href="https://www.thecipherbrief.com/cyber-advisory-board">Meet Our Board</a></h3>
-          <div class="meet_content" style="padding: 4px 4px 0 4px;">
-            <img class="board_img" src="https://thecipherbriefcyber.com/wp-content/uploads/2017/10/Robert-Work-bw.jpg" alt="Robert Work">
-            <p class="board_text">Mr. Robert Work was the 31st Deputy Secretary of Defense, serving under three different Secretaries across two administrations. Previously, he served as chief executive officer of the Center for a New American Security. From 2009 to 2013, he served as the Under Secretary of the Navy. Mr. Work served on active duty in the U.S. Marine Corps for 27 years, retiring as a Colonel in 2001.</p>
-          </div>
-          <div class="board_link_holder">
-              <a class="board_link" href="https://www.thecipherbrief.com/cyber-advisory-board" style=" display: block; text-align: center; color: #d52340;">View the rest of the Cyber Advisory Board</a>
-          </div>
-        </div>
-
       </div>
 
       <div class="promo-holder">
